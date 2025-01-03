e35e09e17a6ac8f18a73d791fe86d912335dfef9
who4mos <126173004+who4mos@users.noreply.github.com>
2025-01-03 15:35:49 -0300

    completed machine setup

--- 05_machine-setup/05_02_yeehaw.py
+++ 05_machine-setup/05_02_yeehaw.py
@@ -3,3 +3,7 @@
 # 	I'm a programmer now.
 # 	Yeehaw!
 # 	Coding here I come!
+
+print("I'm a programmer now")
+print("Yeehaw!")
+print("Coding here I come!")
--- 05_machine-setup/05_04_seconds_year.py
+++ 05_machine-setup/05_04_seconds_year.py
@@ -1,2 +1,4 @@
 # Use the interpreter to calculate how many seconds are in a year.
 # Then write the code in this script file below this line.
+
+365 * 24 * 60 * 60
